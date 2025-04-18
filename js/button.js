1f7ff7566ab10ec168859e0490e5f79d62782bf6
GameMakerBras1l <marcelciro2015@gmail.com>
2025-04-18 18:02:58 -0300

    javascript

--- js/button.js
+++ js/button.js
@@ -1,1 +1,35 @@
-console.log("Hello World")
+document.getElementById("btn.01").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Mach_5"
+};
+
+document.getElementById("btn.02").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Mod_Speeder_(Pull-Back_Speeders)"
+};
+
+document.getElementById("btn.03").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/%2720_Toyota_GR_Supra"
+};
+
+document.getElementById("btn.04").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/2004_Mazda_RX-8_Custom"
+};
+
+document.getElementById("btn.05").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Twin_Mill"
+};
+
+document.getElementById("btn.06").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/GT-Scorcher_(Pull-Back_Speeders)"
+};
+
+document.getElementById("btn.07").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Porsche_914_Safari_(Pull-Back_Speeders)"
+};
+
+document.getElementById("btn.08").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Muscle_and_Blown_(Pull-Back_Speeders)"
+};
+
+document.getElementById("btn.09").onclick = function(){
+    window.location.href = "https://hotwheels.fandom.com/wiki/Deora_II"
+};
